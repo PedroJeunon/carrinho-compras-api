2ab3693ef2d8d93186e5d9cf8900c369c664a1d5
Pedro Jeunon <pedrojeunon@gmail.com>
2018-06-03 19:04:53 -0300

    inclusao da anotação para o swagger dos metodos implementados

--- src/main/java/com/pedrocruz/controller/CarrinhoComprasController.java
+++ src/main/java/com/pedrocruz/controller/CarrinhoComprasController.java
@@ -22,6 +22,8 @@ import com.pedrocruz.service.CarrinhoComprasService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(value = "API para carrinho de compras")
 @RestController
@@ -32,31 +34,47 @@ public class CarrinhoComprasController {
 	private CarrinhoComprasService service;
 
 	@ApiOperation(value = "Metodo de listar todos os carrinhos disponiveis")
+	@ApiResponse(code = 200, message = "Retorna os carrinhos criado")
 	@RequestMapping(path = "/listarcarrinhos", method = RequestMethod.GET)
 	public ResponseEntity<Object> listarCarrinhos() {
-		Response<Map<String, CarrinhoCompras>> response = new Response<Map<String, CarrinhoCompras>>();
+		Response<Object> response = new Response<Object>();
 		Optional<Map<String, CarrinhoCompras>> carrinhos = service.buscarCarrinhos();
-		carrinhos.ifPresent(cars -> response.setResponse(cars));
-		return ResponseEntity.ok().body(response);
+		if (carrinhos.isPresent()) {
+			response.setResponse(carrinhos.get());
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+		} else {
+			response.setResponse("Não existem carrinhos criados");
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+		}
 	}
 
 	@ApiOperation(value = "Metodo que lista os produtos de um determinado carrinho por cpf")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Retorna o carrinho por cpf"),
+			@ApiResponse(code = 204, message = "Caso o body tenha algum erro") })
 	@RequestMapping(path = "/listarcarrinho/{cpf}", method = RequestMethod.GET)
 	public ResponseEntity<Object> listarCarrinhoPorCPF(@PathVariable("cpf") String cpf) {
-		Response<CarrinhoCompras> response = new Response<CarrinhoCompras>();
+		Response<Object> response = new Response<Object>();
 		Optional<CarrinhoCompras> carrinho = service.buscarCarrinho(cpf);
-		carrinho.ifPresent(car -> response.setResponse(car));
-		return ResponseEntity.ok().body(response);
+		if (carrinho.isPresent()) {
+			response.setResponse(carrinho.get());
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+		} else {
+			response.setResponse("Não existe carrinho para este CPF: " + cpf);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+		}
 	}
 
 	@ApiOperation(value = "Metodo que cria um carrinho por cpf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Retorna uma mensagem dizendo que o carrinho esta ok para o cpf"),
+			@ApiResponse(code = 400, message = "Caso o body tenha algum erro") })
 	@RequestMapping(path = "/criarcarrinho", method = RequestMethod.POST)
 	public ResponseEntity<Object> criarCarrinhoPorCPF(@Valid @RequestBody UsuarioDTO usuario, BindingResult result) {
 		Response<String> response = new Response<String>();
 
 		if (result.hasErrors()) {
 			result.getAllErrors().forEach(error -> response.setResponse(error.getDefaultMessage()));
-			return ResponseEntity.badRequest().body(response);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 		}
 
 		service.criarCarrinho(usuario.getCpf());
@@ -65,21 +83,26 @@ public class CarrinhoComprasController {
 	}
 
 	@ApiOperation(value = "Metodo de limpa o carrinho por cpf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Retorna uma mensagem invalidando ou nao um carrinho criado"),
+			@ApiResponse(code = 204, message = "Caso nao encontra nenhum carrinho para o cpf"),
+			@ApiResponse(code = 400, message = "Caso o body tenha algum erro") })
 	@RequestMapping(path = "/limparcarrinho", method = RequestMethod.DELETE)
 	public ResponseEntity<Object> limparCarrinhoPorCPF(@Valid @RequestBody UsuarioDTO usuario, BindingResult result) {
 		Response<String> response = new Response<String>();
 
 		if (result.hasErrors()) {
 			result.getAllErrors().forEach(error -> response.setResponse(error.getDefaultMessage()));
-			return ResponseEntity.badRequest().body(response);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 		}
 
 		if (service.invalidarCarrinho(usuario.getCpf())) {
 			response.setResponse("Carrinho excluido para CPF: " + usuario.getCpf());
+			return ResponseEntity.status(HttpStatus.OK).body(response);
 		} else {
 			response.setResponse("Não exste carrinho para este CPF: " + usuario.getCpf());
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
 		}
-		return ResponseEntity.ok().body(response);
 	}
 
 	// TODO - Implementar metodo
